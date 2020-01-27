7b3581a6bc6d5ac46efbd64d2836f7d0add2eea3
jollyjunkaih <42964786+jollyjunkaih@users.noreply.github.com>
2020-01-27 15:09:07 -0500

    Update blink.c

--- blinkpoll/blink.c
+++ blinkpoll/blink.c
@@ -1,5 +1,5 @@
 /*
-** Copyright (c) 2018, Bradley A. Minch
+** Copyright (c) 2018, Bradley A. Minch XD
 ** All rights reserved.
 **
 ** Redistribution and use in source and binary forms, with or without
